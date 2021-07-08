c433ae58d3726c3fb70394742dc8c5899087c5ef
Akshu-Agarwal <akshitagarwal2204@gmail.com>
2021-07-08 15:49:47 +0530

    Update version

--- dcactivity/__init__.py
+++ dcactivity/__init__.py
@@ -9,7 +9,7 @@ features such as Poker Night, Chess, YouTube and many more.
 
 __name__ = 'dcactivity'
 __author__ = 'AkshuAgarwal'
-__version__ = '1.0.0'
+__version__ = 'v1.1'
 __license__ = 'MIT'
 
 from .dcactivity import DCActivity, DCApplication
